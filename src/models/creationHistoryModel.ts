0ce4f8d5544c133e459b98135bc78a122f9c9742
ChuN6868 <kanaircla@gmail.com>
2024-11-22 19:18:25 +0900

    feat:reffer-creation-history

--- src/models/creationHistoryModel.ts
+++ src/models/creationHistoryModel.ts
@@ -2,14 +2,20 @@ import db from "../db";
 
 export type CreationHistory = {
   id?: string;
-  userId: string;
-  cocktailId: string;
+  user_id: string;
+  cocktail_id: string;
 };
 
 export const addCreationHistory = async (creationHistory: CreationHistory) => {
-  await db("CREATION_HISTORY").insert(creationHistory);
+  await db("creation_history").insert(creationHistory);
 };
 
-export const refferCreationHistory = async (creationHistory: any) => {
-  // await db("CREATION_HISTORY").select("*").where({ cocktailId:  });
+export const refferCreationHistory = async (reqParams: any) => {
+  const result = await db("creation_history").select("*").where({ cocktail_id: reqParams.cocktailId, user_id: reqParams.userId });
+  return result
 };
+
+export const deleteCreationHistory = async (cocktail_id: any, user_id: any) => {
+  await db("creation_history").delete().where({ cocktail_id: cocktail_id, user_id: user_id });
+};
+
