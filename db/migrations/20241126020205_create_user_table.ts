6ba3884c2e113ee8d05424457e6f73888d2e56e6
subaru-komura <s-komura@nri.co.jp>
2024-11-26 13:55:21 +0900

    remove id col from user table

--- db/migrations/20241126020205_create_user_table.ts
+++ db/migrations/20241126020205_create_user_table.ts
@@ -2,8 +2,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("user", (table) => {
-    table.increments("id").primary();
-    table.integer("user_id").notNullable();
+    table.increments("user_id").notNullable().primary();
     table.string("first_name").notNullable();
     table.string("last_name").notNullable();
     table.string("email").notNullable();
