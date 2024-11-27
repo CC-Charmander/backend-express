aeca8ac2eb1abf33a8014cbc32c844c9ed53c079
subaru-komura <s-komura@nri.co.jp>
2024-11-27 13:54:22 +0900

    add ingredientsAPI

--- src/controllers/ingredientsController.ts
+++ src/controllers/ingredientsController.ts
@@ -3,5 +3,5 @@ import { refferAllIngredients } from "../models/ingredientsModel";
 
 export const getAllIngredients = async (req: Request, res: Response) => {
   const result = await refferAllIngredients();
-  res.status(200).json(result.map((ingredient) => ingredient.name));
+  res.status(200).json(result.map((ingredient) => ingredient.name)); //材料名だけ配列にして返す
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -10,15 +10,19 @@ import { getAllIngredients } from "../controllers/ingredientsController";
 
 const router = express.Router();
 
+//creation_history
 router.get("/creation_history", getCreationHistory);
 router.post("/creation_history", createCreationHistory);
 router.delete("/creation_history", deleteCreationHistoryController);
 
+//view_history
 router.post("/view_history", createViewHistory);
 
+//recipes
 router.get("/recipes", getRecipes);
 router.post("/recipes", createRecipe);
 
+//ingredients
 router.get("/ingredients", getAllIngredients);
 
 export default router;
